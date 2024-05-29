6e3c2254cc0f4c57810ba77dc338138b488b7d0f
KarloPry <kpaz30@alumnos.uaq.mx>
2024-05-29 08:09:37 -0600

    new images

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,5 @@
 import { ThemedView } from "@/components/ThemedView";
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
@@ -7,11 +7,16 @@ import {
   FlatList,
   Image,
   ScrollView,
+  Button,
+  TouchableHighlight,
+  TouchableOpacity,
 } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { TextInput } from "react-native";
 import AvailableTrip from "@/components/TripsAvailable";
+import { Picker } from "@react-native-picker/picker";
 const Search = () => {
+  const [payment, setPayment] = useState("");
   const DATA = [
     {
       name: "Lucia Sanchez",
@@ -25,7 +30,7 @@ const Search = () => {
     },
   ];
   return (
-    <ThemedView style={{ flex: 1 }}>
+    <ThemedView style={{ flex: 1, backgroundColor: "" }}>
       <ThemedView
         style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
       >
@@ -42,18 +47,69 @@ const Search = () => {
           <ThemedText style={{ textAlign: "left" }} type="subtitle">
             Transportes disponibles:
           </ThemedText>
-          <ScrollView>
+          <ScrollView
+            style={{ backgroundColor: "#F6F7F9", gap: 10, paddingVertical: 20 }}
+          >
             <AvailableTrip
               id="WWW"
               name="Lucia Sanchez"
               time="14:00"
               destination="Facultad de informática"
               picture={
-                "https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg"
+                "https://raw.githubusercontent.com/KarloPry/troyacar/main/assets/images/trips/lucia.png"
+              }
+            />
+            <AvailableTrip
+              id="WWWWW"
+              name="Juan Olguin"
+              time="15:00"
+              destination="Facultad de informática"
+              picture={
+                "https://raw.githubusercontent.com/KarloPry/troyacar/main/assets/images/trips/juan.png"
               }
             />
           </ScrollView>
         </ThemedView>
+        <ThemedView
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 20,
+            gap: 20,
+          }}
+        >
+          <ThemedView
+            style={{
+              backgroundColor: "#F6F7F9",
+              padding: 10,
+              borderRadius: 10,
+            }}
+          >
+            <ThemedText type="subtitle">Pago:</ThemedText>
+            <TextInput placeholder="$00.00" style={{ fontSize: 20 }} />
+          </ThemedView>
+          <ThemedView>
+            <Picker
+              style={{ width: 200, backgroundColor: "#F6F7F9" }}
+              selectedValue={payment}
+              onValueChange={(itemValue) => setPayment(itemValue)}
+            >
+              <Picker.Item label="Efectivo" value="efectivo" />
+              <Picker.Item label="Tarjeta" value="tarjeta" />
+            </Picker>
+          </ThemedView>
+        </ThemedView>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#1A5FEF",
+            paddingVertical: 16,
+            paddingHorizontal: 40,
+            borderRadius: 10,
+          }}
+        >
+          <ThemedText style={{ color: "white", fontSize: 20 }}>Agendar</ThemedText>
+        </TouchableOpacity>
       </ThemedView>
     </ThemedView>
   );
